3236854a9d38a10b1a3feebc35258e1ce2e0c5c6
Indra Doerga <0957822@hr.nl>
2022-04-19 01:54:20 +0200

    spaces

--- dev/game.ts
+++ dev/game.ts
@@ -106,6 +106,7 @@ class Game {
 
         // AI needs to make a move if it is not the player's turn
         if (!this.playerTurn) {
+            
             setTimeout(() => { // Knights move after Gandalf
             GameAI.moveKnight(this.king, this.knights, this.gameState);
             }, 2000)
